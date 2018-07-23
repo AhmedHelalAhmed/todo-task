71cc8d67a0997f8334f3896b9f57bc4657b4b9da
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-07-23 11:15:50 +0200

    add auth:api to API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,11 +18,11 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('tasks','Api\TasksController@index')->middleware('cors');
-Route::post('tasks','Api\TasksController@store')->middleware('cors');
-Route::delete('tasks/{id}', 'Api\TasksController@destroy')->middleware('cors');
-Route::get('/tasks/{id}', 'Api\TasksController@show')->middleware('cors');
-Route::get('/tasks/{id}/edit', 'Api\TasksController@edit')->middleware('cors');
-Route::put('/tasks/{id}', 'Api\TasksController@update')->middleware('cors');
+Route::get('tasks','Api\TasksController@index')->middleware('cors')->middleware('auth:api');
+Route::post('tasks','Api\TasksController@store')->middleware('cors')->middleware('auth:api');
+Route::delete('tasks/{id}', 'Api\TasksController@destroy')->middleware('cors')->middleware('auth:api');
+Route::get('/tasks/{id}', 'Api\TasksController@show')->middleware('cors')->middleware('auth:api');
+Route::get('/tasks/{id}/edit', 'Api\TasksController@edit')->middleware('cors')->middleware('auth:api');
+Route::put('/tasks/{id}', 'Api\TasksController@update')->middleware('cors')->middleware('auth:api');
 //to swap the elements in case of up and down in frontend
-Route::post('tasks/{id1}/{id2}', 'Api\TasksController@swap')->middleware('cors');
+Route::post('tasks/{id1}/{id2}', 'Api\TasksController@swap')->middleware('cors')->middleware('auth:api');
